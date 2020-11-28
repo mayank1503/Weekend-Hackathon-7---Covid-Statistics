66611ec054677298fe9adb8fc557751a70d5316b
mayank1503 <mayankmotwani1503@gmail.com>
2020-11-28 19:28:47 +0530

    Test

--- src/index.js
+++ src/index.js
@@ -2,69 +2,72 @@ const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
 const port = 8080
-
-// Parse JSON bodies (as sent by API clients)
+let data = require('./data.js');
+data = data['data'];
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 const { connection } = require('./connector')
-
-app.get("/totalRecovered", (req, res) => {
-    let count = 0;
-    connection.find().then(post => {
-        post.map(element => {
-            count += element.recovered;
-        });
-        res.send({data: {_id: "total", recovered:count}});
-    })
+app.get('/totalRecovered', (req, res) => {
+    let ans = 0;
+    for (let i = 0; i < data.length; i++) {
+        let k = data[i];
+        ans += Number(k['recovered']);
+    }
+    res.json({ data: { _id: 'total', recovered: ans } });
 })
 
-app.get("/totalActive", (req, res) => {
-    let count = 0;
-    connection.find().then(post => {
-        post.map(element => {
-            count += (element.infected - element.recovered);
-        });
-        res.send({data: {_id: "total", active:count}});
-    })
-});
+app.get('/totalActive', (req, res) => {
+    let infected = 0;
+    let recovered = 0;
+    for (let i = 0; i < data.length; i++) {
+        let k = data[i];
+        recovered += Number(k['recovered']);
+        infected += Number(k['infected']);
+    }
+    res.json({ data: { _id: 'total', active: (infected - recovered) } });
+})
 
-app.get("/totalDeath", (req, res) => {
-    let count = 0;
-    connection.find().then(post => {
-        post.map(element => {
-            count += (element.death);
-        });
-        res.send({data: {_id: "total", death:count}});
-    })
-});
+app.get('/totalDeath', (req, res) => {
+    let dead = 0;
+    for (let i = 0; i < data.length; i++) {
+        let k = data[i];
+        dead += Number(k['death']);
+    }
+    res.json({ data: { _id: 'total', death: dead } });
+})
 
-app.get("/hotspotStates", (req, res) => {
-    let count = 0;
-    let arr = [];
-    connection.find().then(post => {
-        post.map(element => {
-            let rate = ((element.infected - element.recovered)/ element.infected).toFixed(5);
-            if( rate > 0.1){
-                arr = [...arr, {state: element.state, rate: rate}] 
-            }
-        })
-        return arr;
-    }).then(arr => res.send({data: arr}));
-});
 
-app.get("/healthyStates", (req, res) => {
-    let count = 0;
-    let arr = [];
-    connection.find().then(post => {
-        post.map(element => {
-            let rate = ((element.infected - element.recovered)/ element.infected).toFixed(5);
-            if( rate < 0.005){
-                arr = [...arr, {state: element.state, rate: rate}] 
-            }
-        })
-        return arr;
-    }).then(arr => res.send({data: arr}));
-});
+app.get('/hotspotStates', (req, res) => {
+    let dead = [];
+    for (let i = 0; i < data.length; i++) {
+        let k = data[i];
+        let infected = k['infected'];
+        let recovered = k['recovered'];
+        let diff = Number(infected) - Number(recovered);
+        diff = (diff / Number(infected));
+        diff = diff.toFixed(5);
+        console.log(diff);
+        if (diff > 0.1) {
+            dead.push({ state: k['state'], rate: diff });
+        }
+    }
+    res.json({ data: dead });
+})
+
+app.get('/healthyStates', (req, res) => {
+    let dead = [];
+    for (let i = 0; i < data.length; i++) {
+        let k = data[i];
+        let infected = k['infected'];
+        let death = k['death'];
+        let diff = (Number(death) / Number(infected));
+        diff = diff.toFixed(5);
+        if (diff < 0.005) {
+            dead.push({ state: k['state'], mortality: diff });
+        }
+    }
+    res.json({ data: dead });
+})
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
